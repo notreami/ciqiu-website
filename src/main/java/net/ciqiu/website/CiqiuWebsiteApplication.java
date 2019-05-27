e740197d16b6e59f3ed5004802867b21a1808592
notreami <notreami@163.com>
2019-05-27 23:48:04 +0800

    first commit

---
+++ src/main/java/net/ciqiu/website/CiqiuWebsiteApplication.java
@@ -0,0 +1,13 @@
+package net.ciqiu.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiqiuWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiqiuWebsiteApplication.class, args);
+	}
+
+}
